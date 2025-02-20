1ddc88a37dbc3fd426a4de9eeb57c4ecdc78c244
Ubuntu <amandinancy16@gmail.com>
2025-02-20 12:41:01 +0000

    added the semantics path

--- cube_project/model/globals.py
+++ cube_project/model/globals.py
@@ -4,8 +4,11 @@ import os
 
 template = TemplateContext()
 
-script_dir = os.path.dirname(os.path.realpath(__file__))
-dbt_semantics_path = os.path.join(script_dir, 'dbt_semantics', 'manifest.json')
+# Get the current working directory
+current_dir = os.path.dirname(os.path.realpath(__file__))  # Location of globals.py
+
+# Construct the absolute path to the manifest.json file
+manifest_path = os.path.join(current_dir, 'dbt_semantics', 'manifest.json')
 
 dbt = Dbt.from_file(dbt_semantics_path).filter(paths=['staging/'])
 
