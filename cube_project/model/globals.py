537a2c855d68f1a4ddad4c3a182712373f746da7
Ubuntu <amandinancy16@gmail.com>
2025-02-20 12:37:40 +0000

    added the semantics path

--- cube_project/model/globals.py
+++ cube_project/model/globals.py
@@ -1,9 +1,13 @@
 from cube import TemplateContext
 from cube_dbt import Dbt
+import os
 
 template = TemplateContext()
 
-dbt = Dbt.from_file('./dbt_semantics/manifest.json').filter(paths=['staging/'])
+script_dir = os.path.dirname(os.path.realpath(__file__))
+dbt_semantics_path = os.path.join(script_dir, 'dbt_semantics', 'manifest.json')
+
+dbt = Dbt.from_file(dbt_semantics_path).filter(paths=['staging/'])
 
 for model in dbt.models:
   print(model.name)
